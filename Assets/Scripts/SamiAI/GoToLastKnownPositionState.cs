2ceba79992dc247ed4ab542474c2f0ea86f7612f
Jaakko <jaakko.liukkala@cs.tamk.fi>
2018-04-11 05:29:28 +0300

    Made a simple main menu and end menu for the build. Added the scenes that are to be in the upcoming build to the build scene hierarchy. Did some fiddling with the UI stuff in scenes TestSceneHenkka4 and 5. Added an enemy and a key to TestSceneHenkka5.

--- Assets/Scripts/SamiAI/GoToLastKnownPositionState.cs
+++ Assets/Scripts/SamiAI/GoToLastKnownPositionState.cs
@@ -55,7 +55,7 @@ namespace Invector.AI
 			{
                 enemy.StopAllCoroutines();
                 moveAgain = false;
-                enemy.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                //enemy.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
                 return enemy.PerformTransition( AIStateType.Patrol );
 			}
 			// Otherwise return false.
